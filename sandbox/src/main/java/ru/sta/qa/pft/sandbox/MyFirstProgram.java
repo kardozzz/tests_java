47b92832fee2567cce59d268683d5747e20ddc52
kardozzz <115778040+kardozzz@users.noreply.github.com>
2022-10-27 00:05:17 +0300

    функция
    функция перенесена в класс

--- sandbox/src/main/java/ru/sta/qa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sta/qa/pft/sandbox/MyFirstProgram.java
@@ -1,16 +1,13 @@
 package ru.sta.qa.pft.sandbox;
 
+import static ru.sta.qa.pft.sandbox.Point.area;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		Point a = new Point(2.2,1.5);
-		Point b = new Point(5.5,2.5);
+		Point a = new Point(2.2, 1.5);
+		Point b = new Point(5.5, 2.5);
 
-		System.out.println("Расстояние между точкой а и точкой b = " + area(a,b));
-
+		System.out.println("Расстояние между точкой а и точкой b = " + area(a, b));
 	}
-	public static double area(Point a,Point b){
-		return Math.sqrt((((a.B - b.B) * (a.B - b.B) + (a.A - b.A) * (a.A - b.A))));
-	}
-
 }
