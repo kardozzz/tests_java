6f350ceb32eb458b7ae2d74de02cff9e580a7928
kardozzz <115778040+kardozzz@users.noreply.github.com>
2022-10-26 23:17:46 +0300

    вопрос по задаче

--- sandbox/src/main/java/ru/sta/qa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sta/qa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,16 @@
 package ru.sta.qa.pft.sandbox;
 
 public class MyFirstProgram {
-	
+
 	public static void main(String[] args) {
-	
-System.out.println("Hello World!");
+		Point a = new Point(2.2,5.5);
+		Point b = new Point(5.5,5.5);
+
+		System.out.println("Расстояние между точкой а и точкой b = " + area(p));
+
 	}
+	public static double area(Point p){
+		return Math.sqrt((((p.A + p.B) * (p.A + p.B) - (p.A + p.B) * (p.A + p.B))));
+	}
+
 }
